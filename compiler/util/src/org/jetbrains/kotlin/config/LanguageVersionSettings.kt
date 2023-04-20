d7b16b312fdf0af2522fc640977aa10291f3083c
Alexander Udalov <alexander.udalov@jetbrains.com>
2023-04-20 11:07:41 +0200

    Enable lightweight lambdas (aka invokedynamic) since 2.0
     #KT-45375 Fixed
     #KT-58173 Open

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -292,6 +292,7 @@ enum class LanguageFeature(
     ProhibitOpenValDeferredInitialization(KOTLIN_2_0, kind = BUG_FIX), // KT-57553
     ProhibitMissedMustBeInitializedWhenThereIsNoPrimaryConstructor(KOTLIN_2_0, kind = BUG_FIX), // KT-58472
     MangleCallsToJavaMethodsWithValueClasses(KOTLIN_2_0, kind = OTHER), // KT-55945
+    LightweightLambdas(KOTLIN_2_0), // KT-45375
 
     // 2.1
 
@@ -325,7 +326,6 @@ enum class LanguageFeature(
     // Experimental features
 
     BreakContinueInInlineLambdas(null), // KT-1436
-    LightweightLambdas(null),
     JsEnableExtensionFunctionInExternals(null, kind = OTHER),
     PackagePrivateFileClassesWithAllPrivateMembers(null), // Disabled until the breaking change is approved by the committee, see KT-10884.
     BooleanElvisBoundSmartCasts(null), // see KT-26357 for details
