e9e9bbb2e7200d1a2da38c640346ce4b2faeb896
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2023-04-27 17:53:33 +0200

    Fir2Ir scripts: fix declarations registration and processing

--- compiler/testData/diagnostics/tests/script/resolveInitializerOfDestructuringDeclarationOnce.fir.kts
+++ compiler/testData/diagnostics/tests/script/resolveInitializerOfDestructuringDeclarationOnce.fir.kts
@@ -1,4 +1,4 @@
-val (a, b, c) = <!COMPONENT_FUNCTION_MISSING, COMPONENT_FUNCTION_MISSING, COMPONENT_FUNCTION_MISSING, NO_VALUE_FOR_PARAMETER!>A()<!>
+val (a, b, c) = <!NO_VALUE_FOR_PARAMETER!>A()<!>
 
 class A(val a: Int) {
     operator fun component1() {}
