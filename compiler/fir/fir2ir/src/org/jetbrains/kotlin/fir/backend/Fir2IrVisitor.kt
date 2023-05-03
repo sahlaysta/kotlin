cdd33f0e9de54fe60f44d1943d0959e8eee155e8
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2023-05-03 15:54:13 +0200

    K2 Scripting: add support for script implicit receivers

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -208,22 +208,33 @@ class Fir2IrVisitor(
             irScript.parent = conversionScope.parentFromStack()
             declarationStorage.enterScope(irScript)
 
+            var paramsIndex = script.parameters.size - 1
+
             irScript.explicitCallParameters = script.parameters.map { parameter ->
                 declarationStorage.createIrVariable(parameter, irScript, givenOrigin = IrDeclarationOrigin.SCRIPT_CALL_PARAMETER)
             }
 
-            irScript.thisReceiver = script.contextReceivers.find { it.customLabelName?.asString() == SCRIPT_SPECIAL_NAME_STRING }?.let { receiver ->
-                receiver.convertWithOffsets { startOffset, endOffset ->
-                    irFactory.createValueParameter(
-                        startOffset, endOffset, IrDeclarationOrigin.INSTANCE_RECEIVER, SpecialNames.THIS, receiver.typeRef.toIrType(),
-                        isAssignable = false, IrValueParameterSymbolImpl(), UNDEFINED_PARAMETER_INDEX,
-                        varargElementType = null,
-                        isCrossinline = false, isNoinline = false,
-                        isHidden = false,
-                    ).also {
-                        it.parent = irScript
+            irScript.implicitReceiversParameters = script.contextReceivers.mapNotNull { receiver ->
+                val isSelf = receiver.customLabelName?.asString() == SCRIPT_SPECIAL_NAME_STRING
+                val currentIndex = if (isSelf) UNDEFINED_PARAMETER_INDEX else paramsIndex++
+                val name =
+                    if (isSelf) SpecialNames.THIS
+                    else Name.identifier("${receiver.labelName?.asStringStripSpecialMarkers() ?: SCRIPT_RECEIVER_NAME_PREFIX}_$currentIndex")
+                val origin = if (isSelf) IrDeclarationOrigin.INSTANCE_RECEIVER else IrDeclarationOrigin.SCRIPT_IMPLICIT_RECEIVER
+                val irReceiver =
+                    receiver.convertWithOffsets { startOffset, endOffset ->
+                        irFactory.createValueParameter(
+                            startOffset, endOffset, origin, name, receiver.typeRef.toIrType(), isAssignable = false,
+                            IrValueParameterSymbolImpl(), currentIndex,
+                            varargElementType = null, isCrossinline = false, isNoinline = false, isHidden = false
+                        ).also {
+                            it.parent = irScript
+                        }
                     }
-                }
+                if (isSelf) {
+                    irScript.thisReceiver = irReceiver
+                    null
+                } else irReceiver
             }
 
             conversionScope.withParent(irScript) {
