47c92cc9b221b2ed56b44d2868cd6d65d520400a
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-06-19 14:31:39 +0200

    fixup! [FE 1.0] Prohibit implicit Java actualization in K1

--- core/descriptors/src/org/jetbrains/kotlin/mpp/MppJavaImplicitActualizatorMarker.kt
+++ core/descriptors/src/org/jetbrains/kotlin/mpp/MppJavaImplicitActualizatorMarker.kt
@@ -5,4 +5,10 @@
 
 package org.jetbrains.kotlin.mpp
 
+/**
+ * The marker interface marks all Java descriptors that can implicitly actualize expect declarations.
+ *
+ * The marker interface is needed for being able to access information about Java specific descriptors from platform-agnostic
+ * `:compiler:frontend` module
+ */
 interface MppJavaImplicitActualizatorMarker
