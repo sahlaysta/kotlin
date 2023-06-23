5d30f435546f0a73f564e267cb2f41f8c6b0461c
Roman Efremov <roman.efremov@jetbrains.com>
2023-06-23 13:06:21 +0200

    [IR] Don't check annotations on expect/actual fake overrides
    ^KT-58551

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/actualizer/IrExpectActualAnnotationMatchingChecker.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/actualizer/IrExpectActualAnnotationMatchingChecker.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.ir.symbols.IrSymbol
 import org.jetbrains.kotlin.ir.symbols.IrTypeAliasSymbol
 import org.jetbrains.kotlin.ir.types.IrTypeSystemContext
 import org.jetbrains.kotlin.ir.util.classIdOrFail
+import org.jetbrains.kotlin.ir.util.isFakeOverride
 import org.jetbrains.kotlin.resolve.calls.mpp.AbstractExpectActualAnnotationMatchChecker
 
 internal class IrExpectActualAnnotationMatchingChecker(
@@ -34,6 +35,9 @@ internal class IrExpectActualAnnotationMatchingChecker(
 
     fun check() {
         for ((expectSymbol, actualSymbol) in matchedExpectToActual.entries) {
+            if (expectSymbol.isFakeOverride || actualSymbol.isFakeOverride) {
+                continue
+            }
             if (!AbstractExpectActualAnnotationMatchChecker.areAnnotationsCompatible(expectSymbol, actualSymbol, context)) {
                 val reportOn = getTypealiasSymbolIfActualizedViaTypealias(expectSymbol) ?: actualSymbol
                 diagnosticsReporter.reportActualAnnotationsNotMatchExpect(
@@ -44,6 +48,9 @@ internal class IrExpectActualAnnotationMatchingChecker(
             }
         }
     }
+
+    private val IrSymbol.isFakeOverride: Boolean
+        get() = (owner as IrDeclaration).isFakeOverride
 
     private fun getTypealiasSymbolIfActualizedViaTypealias(expectSymbol: IrSymbol): IrTypeAliasSymbol? {
         val expectDeclaration = expectSymbol.owner as IrDeclaration
---
+++ compiler/testData/diagnostics/tests/multiplatform/annotationMatching/fakeOverrides.kt
@@ -0,0 +1,34 @@
+// FIR_IDENTICAL
+// MODULE: m1-common
+// FILE: common.kt
+annotation class Ann
+
+interface I {
+    @Ann
+    fun noAnnotationOnActual()
+}
+
+expect class FakeOverrideExpect : I
+
+interface I2 {
+    fun noAnnotationOnActual()
+}
+
+expect class FakeOverrideActual : I2 {
+    @Ann
+    override fun noAnnotationOnActual()
+}
+
+// MODULE: m1-jvm()()(m1-common)
+// FILE: jvm.kt
+actual class FakeOverrideExpect : I {
+    override fun noAnnotationOnActual() {}
+}
+
+abstract class Intermediate : I2 {
+    override fun noAnnotationOnActual() {}
+}
+
+class FakeOverrideActualImpl : Intermediate(), I2
+
+actual typealias FakeOverrideActual = FakeOverrideActualImpl
