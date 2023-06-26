67bee5d6a3f47c7e9b50255ef99bd05decdcc719
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-06-26 14:42:17 +0300

    [FIR] Set source on return expression in generated getters

--- compiler/fir/raw-fir/raw-fir.common/src/org/jetbrains/kotlin/fir/builder/ConversionUtils.kt
+++ compiler/fir/raw-fir/raw-fir.common/src/org/jetbrains/kotlin/fir/builder/ConversionUtils.kt
@@ -441,6 +441,7 @@ fun <T> FirPropertyBuilder.generateAccessorsByDelegate(
                         origin = FirFunctionCallOrigin.Operator
                     }
                     target = returnTarget
+                    source = fakeSource
                 }
             )
             if (annotations != null) {
